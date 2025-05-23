dcbea74f43146099635f6760266f744160d623b7
mongarerobert3 <mongarerobert3@gmail.com>
2025-05-23 13:26:57 +0300

    fix: Dockerfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Allow CORS for frontend development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://courte-podcast.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
